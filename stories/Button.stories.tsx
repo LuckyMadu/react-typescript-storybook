493c13eb795033025d00b659286705762e410af9
Lahiru Amaratunge <lahiru.a@keeneye.solutions>
2021-12-13 20:18:51 +0530

    first commit

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Button, Props } from '../src/Button';
+
+const meta: Meta = {
+  title: 'Button',
+  component: Button,
+};
+
+export default meta;
+
+export const Default = () => <Button variant="primary">CLICK ME</Button>;
+
+export const Secondary = () => <Button variant="secondary">CLICK ME</Button>;
